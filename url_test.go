5e22d0bec80487360c7077a39ddae6e93a498e29
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-07-30 06:43:35 -0600

    gofmt.

--- url_test.go
+++ url_test.go
@@ -8,5 +8,5 @@ func TestURL(t *testing.T) {
 	assertEqual("https://asdf.com/path?a=1&b=2", URL("https", "asdf.com", "/path", "a", "1", "b", "2").String(), t)
 	assertEqual(URLParsed("https://asdf.com/path?a=1&b=2").String(), URL("https", "asdf.com", "/path", "a", "1", "b", "2").String(), t)
 	assertPanic(func() { URL("https", "asdf.com", "/path", "a", "1", "b") }, t) // incorrect number of query string parameters
-	assertPanic(func() { URLParsed("/%%28") }, t) // malformed URL
+	assertPanic(func() { URLParsed("/%%28") }, t)                               // malformed URL
 }
