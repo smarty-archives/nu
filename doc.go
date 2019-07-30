64ae9387cc7459b47b99bf3be0a79c35d4127138
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-07-30 06:45:35 -0600

    Golint.

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// package nu implements convenient constructor functions for commonly used standard library types.
+// Package nu implements convenient constructor functions for commonly used standard library types.
 package nu
